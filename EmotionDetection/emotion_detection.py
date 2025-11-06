5a9a9c2d500f80fc761acb17ada6e094adaa9550
Dov Rinkoff <dubbyrinkoff@gmail.com>
2025-11-06 15:57:54 -0500

    some fixes

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -25,5 +25,5 @@ def emotion_detector(text_to_analyze):
         'fear': fear_score,
         'joy': joy_score,
         'sadness': sadness_score,
-    'dominant_emotion': dominant_emotion
+        'dominant_emotion': dominant_emotion
         }
--- server.py
+++ server.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/emotionDetector")
-def emotion_detector():
+def em_detector():
     text_to_analyze = request.args.get("textToAnalyze")
 
-    emotion = emotion_detector(textToAnalyze)
+    emotion = emotion_detector(text_to_analyze)
 
-    return f"TFor the given statement, the system response is 'anger': {emotion['anger']}, " + \
+    return f"For the given statement, the system response is 'anger': {emotion['anger']}, " + \
         f"'disgust': {emotion['disgust']}, 'fear': {emotion['fear']}, 'joy': {emotion['joy']} and 'sadness': {emotion['sadness']}." + \
-        f"The dominant emotion is {emotion['dominent_emotion']}."
+        f"The dominant emotion is {emotion['dominant_emotion']}."
 
 @app.route("/")
 def index():
